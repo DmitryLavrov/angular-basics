a5cff57f5f8ae9e7221c4d26c299f6a7e84dae81
DmitryLavrov <d3236562@mail.ru>
2020-11-28 15:18:24 +0300

    3.6. Доступ к html-элементу: this.infoRef.nativeElement

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,18 +1,21 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ContentChild, ElementRef, Input, OnInit} from '@angular/core';
 import {Post} from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent implements OnInit {
 
-  @Input() post: Post
+  @Input() post: Post;
+  @ContentChild('info', {static: true}) infoRef: ElementRef;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    console.log(this.infoRef.nativeElement);
   }
 
 }
