1d9acff64c1ac4538c344160c72aed904696ecb8
DmitryLavrov <d3236562@mail.ru>
2020-11-29 16:56:35 +0300

    4.5. Изменяем те же атрибуты с помощью декоратора @HostBinding

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,34 +1,41 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, HostBinding, HostListener, Input} from '@angular/core';
 
 @Directive({
-  selector: '[appStyle]'
+  selector: '[appStyle]',
 })
-export class StyleDirective{
-  @Input('appStyle') color: string = 'blue'
-  @Input() fontWeight? = 'normal'
-  @Input() dStyle: {border?:string, borderRadius?: string}
+export class StyleDirective {
+  @Input('appStyle') color: string = 'blue';
+  @Input() fontWeight? = 'normal';
+  @Input() dStyle: { border?: string, borderRadius?: string };
 
-  constructor(private  el: ElementRef, private renderer: Renderer2) {
-    console.log('el', el);
-    // this.renderer.setStyle(this.el.nativeElement, 'color', 'darkblue')
-    // el.nativeElement.style.color = 'darkred'
-  }
+  @HostBinding('style.color') elColor = null;
+  @HostBinding('style.fontWeight') elFontWeight = null;
+  @HostBinding('style.border') elBorder = null;
+  @HostBinding('style.borderRadius') elBorderRadius = null;
 
   @HostListener('click', ['$event.target']) onClick(event: Event) {
-    console.log('event',event);
+    console.log('event', event);
   }
 
   @HostListener('mouseenter') onEnter() {
-    this.renderer.setStyle(this.el.nativeElement, 'color', this.color)
-    this.renderer.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
-    this.renderer.setStyle(this.el.nativeElement, 'border', this.dStyle.border)
-    this.renderer.setStyle(this.el.nativeElement, 'borderRadius', this.dStyle.borderRadius)
+    this.elColor = this.color;
+    this.elFontWeight = this.fontWeight;
+    this.elBorder = this.dStyle.border;
+    this.elBorderRadius = this.dStyle.borderRadius;
+    // this.renderer.setStyle(this.el.nativeElement, 'color', this.color)
+    // this.renderer.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight)
+    // this.renderer.setStyle(this.el.nativeElement, 'border', this.dStyle.border)
+    // this.renderer.setStyle(this.el.nativeElement, 'borderRadius', this.dStyle.borderRadius)
   }
 
   @HostListener('mouseleave') onLeave() {
-    this.renderer.setStyle(this.el.nativeElement, 'color', null)
-    this.renderer.setStyle(this.el.nativeElement, 'fontWeight', null)
-    this.renderer.setStyle(this.el.nativeElement, 'border', null)
-    this.renderer.setStyle(this.el.nativeElement, 'borderRadius', null)
+    this.elColor = null;
+    this.elFontWeight = null;
+    this.elBorder = null;
+    this.elBorderRadius = null;
+    // this.renderer.setStyle(this.el.nativeElement, 'color', null)
+    // this.renderer.setStyle(this.el.nativeElement, 'fontWeight', null)
+    // this.renderer.setStyle(this.el.nativeElement, 'border', null)
+    // this.renderer.setStyle(this.el.nativeElement, 'borderRadius', null)
   }
 }
