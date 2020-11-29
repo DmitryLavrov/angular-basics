0c287f20f9ab6bbedc032c13f9b0a3d2f26d0819
DmitryLavrov <d3236562@mail.ru>
2020-11-29 17:43:50 +0300

    4.6. Создаем структурную директиву appIfNot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {StyleDirective} from './directives/style.directive';
+import { IfNotDirective } from './directives/ifnot.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDirective
+    StyleDirective,
+    IfNotDirective
   ],
   imports: [
     BrowserModule,
