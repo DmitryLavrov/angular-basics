1cd4b1211dcd182f48eed199f3cb1d86195b432d
DmitryLavrov <d3236562@mail.ru>
2020-11-29 15:38:00 +0300

    4.2. Меняем стиль элемента в конструкторе

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,12 @@
-import {Directive} from '@angular/core';
+import {Directive, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective{
-
+  constructor(private  el: ElementRef, private renderer: Renderer2) {
+    console.log('el', el);
+    this.renderer.setStyle(this.el.nativeElement, 'color', 'darkblue')
+    // el.nativeElement.style.color = 'darkred'
+  }
 }
