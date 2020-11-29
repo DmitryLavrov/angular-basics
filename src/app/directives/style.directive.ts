1f707063fc1fa3aa6ef6a7f55d2f32126db872da
DmitryLavrov <d3236562@mail.ru>
2020-11-29 16:03:31 +0300

    4.3. Изменение стиля при событии (наведение мыши)

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -6,7 +6,19 @@ import {Directive, ElementRef, Renderer2} from '@angular/core';
 export class StyleDirective{
   constructor(private  el: ElementRef, private renderer: Renderer2) {
     console.log('el', el);
-    this.renderer.setStyle(this.el.nativeElement, 'color', 'darkblue')
+    // this.renderer.setStyle(this.el.nativeElement, 'color', 'darkblue')
     // el.nativeElement.style.color = 'darkred'
+  }
+
+  @HostListener('click', ['$event.target']) onClick(event: Event) {
+    console.log('event',event);
+  }
+
+  @HostListener('mouseenter') onEnter() {
+    this.renderer.setStyle(this.el.nativeElement, 'color', 'darkblue')
+  }
+
+  @HostListener('mouseleave') onLeave() {
+    this.renderer.setStyle(this.el.nativeElement, 'color', null)
   }
 }
