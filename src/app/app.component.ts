5d57fb2f34a6a1368f9398f105f3140810957792
DmitryLavrov <d3236562@mail.ru>
2020-11-29 21:09:17 +0300

    5.5. Встроенные пайпы для чисел (currency, percent) и объектов (json)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,17 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-  date: Date = new Date()
+  float: Number = 0.42
+
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e:3,
+        f:4
+      }
+    }
+  }
 }
 
