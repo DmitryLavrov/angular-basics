cb46b83b98f93c0c0ee440a1b4553734ab25fe88
DmitryLavrov <d3236562@mail.ru>
2020-12-01 21:14:20 +0300

    6.1. Простой сервис AppCounterService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Component} from '@angular/core';
+import {AppCounterService} from './services/app-counter.service';
 
-export interface Posts {
-  title: String,
-  text: String
-}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent  {
 
-  p: Promise<string> = new Promise<string>(resolve => {
-    setTimeout(() =>{
-      resolve('Promise Resolved')
-    }, 4000)
-  })
+  constructor(public appCounterService:AppCounterService) {
 
-  date$: Observable<Date> = new Observable<Date>(obs => {
-    setInterval(()=>{
-      obs.next(new Date())
-    }, 1000)
-  })
-
-  // date: Date
-
-  ngOnInit() {
-    // this.date$.subscribe(date => {
-    //   this.date = date
-    // })
   }
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,20 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {MultByPipe} from './pipes/mult-by.pipe';
-import { ExMarksPipe } from './pipes/ex-marks.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
+import {AppCounterService} from './services/app-counter.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe,
-    ExMarksPipe,
-    FilterPipe
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    AppCounterService
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
