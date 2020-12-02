734d603a92591ea1fd8987ce6fc148ec6780706d
DmitryLavrov <d3236562@mail.ru>
2020-12-02 21:54:47 +0300

    7.4. Добавляем вылидацию. Validators.email. И включаем ее на кнопке [disabled]="form.invalid"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +9,27 @@ import {FormControl, FormGroup} from '@angular/forms';
 
 export class AppComponent implements OnInit {
 
-  form: FormGroup
+  form: FormGroup;
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(null)
-    })
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   };
 
   submit() {
-    console.log('Form: ', this.form);
-    const formData = {...this.form.value}
-    console.log('Form Data: ', formData);
+    if (this.form.valid) {
+      console.log('Form: ', this.form);
+      const formData = {...this.form.value};
+      console.log('Form Data: ', formData);
+    }
   }
 }
 
