47e48ffe3df407cb3e92f808754ad625a6236ddb
DmitryLavrov <d3236562@mail.ru>
2020-12-03 12:03:36 +0300

    7.9. Добавление навыков в форму: *ngFor="let control of form.get('skills').controls; let idx = index"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -24,7 +24,8 @@ export class AppComponent implements OnInit {
       address: new FormGroup({
         country: new FormControl('ru'),
         city: new FormControl('', Validators.required)
-      })
+      }),
+      skills: new FormArray([])
     });
   };
 
@@ -47,6 +48,12 @@ export class AppComponent implements OnInit {
     const city = cityMap[cityKey]
     console.log(city);
     this.form.patchValue({address:{city}})
+  }
+
+  addSkill() {
+    const control = new FormControl('',Validators.required);
+      // (<FormArray>this.form.get('skills')).push(control)
+      (this.form.get('skills') as FormArray).push(control)
   }
 }
 
