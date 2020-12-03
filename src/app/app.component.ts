0cc78971ee3a6dcff02cc981811df8e7aa85b027
DmitryLavrov <d3236562@mail.ru>
2020-12-03 18:17:58 +0300

    7.13. Создание своей ngModel (переключатель On-Off)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
-import {MyValidators} from './my.validators';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,57 +6,11 @@ import {MyValidators} from './my.validators';
   styleUrls: ['./app.component.scss'],
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
+  appState = 'on'
 
-  form: FormGroup;
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      email: new FormControl('', [
-        Validators.email,
-        Validators.required,
-        MyValidators.restrictedEmails
-      ],
-        MyValidators.uniqEmail),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6),
-      ]),
-      address: new FormGroup({
-        country: new FormControl('ru'),
-        city: new FormControl('', Validators.required)
-      }),
-      skills: new FormArray([])
-    });
-  };
-
-  submit() {
-    if (this.form.valid) {
-      console.log('Form: ', this.form);
-      const formData = {...this.form.value};
-      console.log('Form Data: ', formData);
-
-      this.form.reset()
-    }
-  }
-
-  setCapital() {
-    const cityMap = {
-      ru: 'Москва',
-      ua: 'Киев',
-      by: 'Минск',
-    }
-
-    const cityKey = this.form.get('address').get('country').value
-    const city = cityMap[cityKey]
-    console.log(city);
-    this.form.patchValue({address:{city}})
-  }
-
-  addSkill() {
-    const control = new FormControl('',Validators.required);
-      // (<FormArray>this.form.get('skills')).push(control)
-      (this.form.get('skills') as FormArray).push(control)
+  handleChange() {
+    console.log(this.appState)
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SwitchComponent,
   ],
   imports: [
     BrowserModule,
