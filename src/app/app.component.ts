ad4ae22b459146fe044f4be754b7da8a68fe43b4
DmitryLavrov <d3236562@mail.ru>
2020-12-04 14:27:12 +0300

    8.4. Кнопка "Загрузить". Добавлена задержка: .pipe(delay( 2000)). И в шаблоне: <ng-template #loadingBlock>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {delay} from 'rxjs/operators';
 
 export interface Todo {
   completed: boolean
@@ -13,37 +14,45 @@ export interface Todo {
   styleUrls: ['./app.component.scss'],
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  todos: Todo[] = []
-  todoTitle = ''
+  todos: Todo[] = [];
+  todoTitle = '';
+  loading = false
 
-  constructor(private  http: HttpClient) {}
+  constructor(private  http: HttpClient) {
+  }
 
   ngOnInit(): void {
-    this.http.get<Todo[]> ('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .subscribe(todos => {
-        console.log('Response: ', todos)
-        this.todos = todos
-      })
+    this.fetchTodos()
   }
 
   addTodo() {
     if (!this.todoTitle.trim()) {
-      return
+      return;
     }
 
     const NewTodo: Todo = {
       title: this.todoTitle,
-      completed: false
-    }
+      completed: false,
+    };
 
-    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos',NewTodo)
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', NewTodo)
       .subscribe(todo => {
-        console.log('Post: ', todo)
-        this.todos.unshift(todo)
-        this.todoTitle = ''
-      })
+        console.log('Post: ', todo);
+        this.todos.unshift(todo);
+        this.todoTitle = '';
+      });
+  }
+
+  fetchTodos() {
+    this.loading = true
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .pipe(delay( 2000))
+      .subscribe(todos => {
+        this.todos = todos;
+        this.loading = false
+      });
   }
 }
 
