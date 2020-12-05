7303df0bf236e6e251e66530be24f33bcf7271fb
DmitryLavrov <d3236562@mail.ru>
2020-12-05 15:09:52 +0300

    9.7. Выводим в шаблон данные для динамического роута: this.rout.params.subscribe. В шаблоне: {{post.title}}

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,33 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Post, PostsService} from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {
 
+export class PostComponent implements OnInit{
+
+  post: Post
+
+  constructor(
+    private rout: ActivatedRoute,
+    private postService: PostsService,
+    private router: Router
+    ) {}
+
+  id = ''
+
+  ngOnInit(): void {
+    this.rout.params.subscribe((params) => {
+      console.log('Params:', params);
+      this.post = this.postService.getById(+params.id)
+    })
+  }
+
+  loadPost() {
+    this.router.navigate(['/post', 44])
+  }
 }
