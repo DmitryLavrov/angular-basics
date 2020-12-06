a8ee056fb720207a76bf7ba272c85a302471d7f6
DmitryLavrov <d3236562@mail.ru>
2020-12-06 20:15:07 +0300

    9.10. Обработка ошибочных роутов: {path: '**', redirectTo: '/error'}

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {AboutComponent} from './about/about.component';
 import {PostsComponent} from './posts/posts.component';
 import {PostComponent} from './post/post.component';
 import {AboutExtraComponent} from './about-extra/about-extra.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -12,7 +13,9 @@ const routes: Routes = [
       {path: 'extra', component: AboutExtraComponent}
     ]},
   {path: 'posts', component: PostsComponent},
-  {path: 'post/:id', component: PostComponent}
+  {path: 'post/:id', component: PostComponent},
+  {path: 'error', component: ErrorPageComponent},
+  {path: '**', redirectTo: '/error'}
 ]
 
 @NgModule({
