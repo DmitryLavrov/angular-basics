33eeceae9859ef7746d57d93c927e11278e1420c
DmitryLavrov <d3236562@mail.ru>
2020-12-06 20:55:54 +0300

    9.11. Кнопки "Login" "Logout". Защита страницы "/posts"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import {PostsComponent} from './posts/posts.component';
 import {PostComponent} from './post/post.component';
 import {AboutExtraComponent} from './about-extra/about-extra.component';
 import {ErrorPageComponent} from './error-page/error-page.component';
+import {AuthGuard} from './auth-guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent, children:[
       {path: 'extra', component: AboutExtraComponent}
     ]},
-  {path: 'posts', component: PostsComponent},
+  {path: 'posts', component: PostsComponent, canActivate: [AuthGuard]},
   {path: 'post/:id', component: PostComponent},
   {path: 'error', component: ErrorPageComponent},
   {path: '**', redirectTo: '/error'}
