3019570dd83e7be5ca3489e185685c3a0aa44a44
DmitryLavrov <d3236562@mail.ru>
2020-12-07 16:46:37 +0300

    10.5. Lazy loading. Новый синтаксис loadChildren.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import {HomePageComponent} from './home-page/home-page.component'
 @NgModule({
   imports: [RouterModule.forRoot([
     {path: '', component: HomePageComponent, pathMatch: 'full'},
-    {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'}
+    // {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'}
+    {path: 'about', loadChildren: () => import('./about-page/about-page.module').then(m => m.AboutPageModule) }
   ])],
   exports: [RouterModule]
 })
