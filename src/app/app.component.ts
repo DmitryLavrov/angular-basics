b8ef278b8d3f28162cc1a7359488f3d972dabf0e
DmitryLavrov <d3236562@mail.ru>
2020-12-07 19:41:04 +0300

    11.2. Модальное окно выводим программно: ComponentFactoryResolver

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import {Component} from '@angular/core'
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
+import {ModalComponent} from './modal/modal.component';
+import { RefDirective } from './ref.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  modal = false
+
+  @ViewChild(RefDirective) refDir:RefDirective
+
+  constructor(private resolver: ComponentFactoryResolver) {}
+
+  showModal() {
+    const modalFactory = this.resolver.resolveComponentFactory(ModalComponent)
+    this.refDir.containerRef.clear()
+    const component = this.refDir.containerRef.createComponent(modalFactory)
+    component.instance.title = 'Dynamic title'
+    component.instance.close.subscribe(() => {
+      this.refDir.containerRef.clear()
+    })
+  }
 }
 
