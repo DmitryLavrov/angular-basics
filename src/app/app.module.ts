b5b7d72a89c381202062f890e59983b9166c8b01
DmitryLavrov <d3236562@mail.ru>
2020-12-07 18:46:03 +0300

    11.1. Создали модальное окно - компонент modal.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,17 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
-import {HomePageComponent} from './home-page/home-page.component'
-import {AppRoutingModule} from './app-routing.module'
-// import {AboutPageModule} from './about-page/about-page.module';
-import {SharedModule} from './shared/shared.module';
+import {FormsModule} from '@angular/forms';
+import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    // AboutPageModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
