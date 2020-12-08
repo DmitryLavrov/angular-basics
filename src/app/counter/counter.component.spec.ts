223cc33c907200dfcf5578fdb5d547da5149f07e
DmitryLavrov <d3236562@mail.ru>
2020-12-08 20:07:02 +0300

    13.2. Проверяем передачу значения counter в шаблон: fixture.debugElement.query(By.css('.counter'))

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,5 +1,6 @@
 import {CounterComponent} from "./counter.component";
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
 describe('CounterComponent', () => {
   let component: CounterComponent
@@ -16,5 +17,14 @@ describe('CounterComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeDefined()
+  });
+
+  it('should render counter property', function() {
+    const num = 42
+    component.counter = num
+    fixture.detectChanges()
+    const de = fixture.debugElement.query(By.css('.counter'))
+    const el: HTMLElement = de.nativeElement
+    expect(el.textContent).toContain(num.toString())
   });
 })
