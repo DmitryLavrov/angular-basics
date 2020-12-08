43c42e427b2103c5af6ac88be86968ad553bf686
DmitryLavrov <d3236562@mail.ru>
2020-12-08 21:17:54 +0300

    13.4. Тестирование сервиса. Проверяем получение posts при ngOnInit

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -1,13 +1,34 @@
 import {PostsComponent} from "./posts.component";
 import {PostsService} from "./posts.service";
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientModule} from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('PostsComponent', () => {
+  let fixture: ComponentFixture<PostsComponent>
   let component: PostsComponent
   let service: PostsService
 
   beforeEach(() => {
-    service = new PostsService(null)
-    component = new PostsComponent(service)
+    TestBed.configureTestingModule({
+      declarations: [PostsComponent],
+      providers: [PostsService],
+      imports: [HttpClientModule]
+    })
+
+    fixture = TestBed.createComponent(PostsComponent)
+    component = fixture.componentInstance
+    // service = fixture.debugElement.injector.get(PostsService)
+    service = TestBed.inject(PostsService)
   })
+
+  it('should fetch posts on ngOnInit', function() {
+    const posts = [1,2,3]
+    spyOn(service, 'fetch').and.returnValue(of(posts))
+
+    fixture.detectChanges()
+
+    expect(component.posts).toEqual(posts)
+  });
 
 })
