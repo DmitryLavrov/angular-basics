dbc22f591392631ab6e4bdbfd3228cfc65525c87
DmitryLavrov <d3236562@mail.ru>
2020-12-09 14:37:15 +0300

    14.2. Состояния анимации: переменная boxState. В шаблоне: [@box]="boxState"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import {state, style, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('box', [
+      state('start', style({background: 'blue'})),
+      state('end',style({
+        background: 'red',
+        transform: 'scale(1.2)'
+      }))
+    ])
+  ]
 })
-export class AppComponent {}
+export class AppComponent {
+  boxState = 'start'
+
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+  }
+}
 
