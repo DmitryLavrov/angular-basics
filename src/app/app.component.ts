d3cf12ee504523794098e435375e117313b6b439
DmitryLavrov <d3236562@mail.ru>
2020-12-09 16:27:19 +0300

    14.7. Добавляем анимацию отдельного объекта: query('h4', animate())

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {animate, group, state, style, transition, trigger} from '@angular/animations';
+import {animate, group, query, sequence, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -20,9 +20,17 @@ import {animate, group, state, style, transition, trigger} from '@angular/animat
       transition('start => end', animate(450)),
       transition('end => start', animate('800ms ease-in-out')),
       transition('special <=> *', [
-        style({background: 'green'}),
-        animate('1s', style({background: 'pink'})),
-        animate(750),
+        group([
+          query('h4', animate(1500, style({
+            fontSize: '0.5rem',
+            color: 'red'
+          }))),
+          sequence([
+            style({background: 'green'}),
+            animate('1s', style({background: 'pink'})),
+            animate(750),
+          ])
+        ])
       ]),
       // void => *
       transition(':enter', [
