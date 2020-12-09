ef557cab46a92fa8b086bf73a3563cc44f1630ca
DmitryLavrov <d3236562@mail.ru>
2020-12-09 16:16:21 +0300

    14.6. Одновременные анимации: group (последовательные анимации: sequence)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import {Component} from '@angular/core';
+import {animate, group, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -8,45 +8,51 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   animations: [
     trigger('box', [
       state('start', style({background: 'blue'})),
-      state('end',style({
+      state('end', style({
         background: 'red',
-        transform: 'scale(1.2)'
+        transform: 'scale(1.2)',
       })),
       state('special', style({
         background: 'orange',
         transform: 'scale(0.5)',
-        borderRadius: '50%'
+        borderRadius: '50%',
       })),
       transition('start => end', animate(450)),
       transition('end => start', animate('800ms ease-in-out')),
       transition('special <=> *', [
         style({background: 'green'}),
         animate('1s', style({background: 'pink'})),
-        animate(750)
+        animate(750),
       ]),
       // void => *
       transition(':enter', [
         style({opacity: 0}),
-        animate('850ms ease-out')
+        animate('850ms ease-out'),
       ]),
       // * => void
       transition(':leave', [
         style({opacity: 1}),
-        animate(750, style({
-          opacity:0,
-          transform:'scale(1.2)'
-        }))
-      ])
-    ])
-  ]
+        group([
+          animate(750, style({
+            opacity: 0,
+            transform: 'scale(1.2)',
+          })),
+          animate(300, style({
+            color: 'black',
+            fontSize: 0
+          }))
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
 
-  boxState = 'start'
-  visible = true
+  boxState = 'start';
+  visible = true;
 
   animate() {
-    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+    this.boxState = this.boxState === 'end' ? 'start' : 'end';
   }
 }
 
