0d7f92c512471f8d7be41281c09d6be2957b35f5
DmitryLavrov <d3236562@mail.ru>
2020-12-09 15:07:39 +0300

    14.3. Анимация: transition - массив щагов

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,18 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
         background: 'red',
         transform: 'scale(1.2)'
       })),
+      state('special', style({
+        background: 'orange',
+        transform: 'scale(0.5)',
+        borderRadius: '50%'
+      })),
       transition('start => end', animate(450)),
-      transition('end => start', animate('800ms ease-in-out'))
+      transition('end => start', animate('800ms ease-in-out')),
+      transition('special <=> *', [
+        style({background: 'green'}),
+        animate('1s', style({background: 'pink'})),
+        animate(750)
+      ]),
     ])
   ]
 })
